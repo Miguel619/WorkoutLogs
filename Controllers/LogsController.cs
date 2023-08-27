80f60668396e3186144584ba4ea7fc2098bb7a8c
16192 <16192@DESKTOP-UTKMA44>
2023-08-27 00:54:27 -0500

    Add project files.

---
+++ Controllers/LogsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DoTheWork.Data;
+using DoTheWork.Models;
+
+namespace DoTheWork.Controllers
+{
+    public class LogsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LogsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Logs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Log.ToListAsync());
+        }
+
+        // GET: Logs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var log = await _context.Log
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (log == null)
+            {
+                return NotFound();
+            }
+
+            return View(log);
+        }
+
+        // GET: Logs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Logs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Activity,Duration,Calories")] Log log)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(log);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(log);
+        }
+
+        // GET: Logs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var log = await _context.Log.FindAsync(id);
+            if (log == null)
+            {
+                return NotFound();
+            }
+            return View(log);
+        }
+
+        // POST: Logs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Activity,Duration,Calories")] Log log)
+        {
+            if (id != log.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(log);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LogExists(log.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(log);
+        }
+
+        // GET: Logs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var log = await _context.Log
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (log == null)
+            {
+                return NotFound();
+            }
+
+            return View(log);
+        }
+
+        // POST: Logs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var log = await _context.Log.FindAsync(id);
+            _context.Log.Remove(log);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LogExists(int id)
+        {
+            return _context.Log.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Log.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoTheWork.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string Activity { get; set; }
+        public string Duration { get; set; }
+        public int Calories { get; set; }
+        public Log()
+        {
+
+        }
+    }
+}
